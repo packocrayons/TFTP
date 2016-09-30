6a9781dbcad87baa08f8482483cda2f3a07a0b50
AdsBerger <Adam.Berg14@hotmail.com>
2016-09-30 14:56:57 -0400

    Update TFTPClient.java

--- src/TFTPClient.java
+++ src/TFTPClient.java
@@ -1,11 +1,16 @@
 import java.net.*;
-//What up boys, brydon boy u went offfff!!!
+import java.awt.List; 
+
 public class TFTPClient extends UDPParent{
 
-	DatagramSocket clientSocket
+	DatagramSocket clientSocket;
 
 	public TFTPClient(){
-		clientSocket = new DatagramSocket();
+		try {
+			clientSocket = new DatagramSocket();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public byte[] createRequestBlock(boolean readRequest, String filename){
@@ -27,18 +32,18 @@ public class TFTPClient extends UDPParent{
 			String filename; /*= filename from gui*/
 
 			byte[] requestArray = client.createRequestBlock(true, filename);
-			DatagramPacket requestPacket = client.generateDatagram(requestArray, client.ipAddress, 23);
+			DatagramPacket requestPacket = client.generateDatagram(requestArray, client.IPAddress, 23);
 			client.sendDatagram(requestPacket, client.clientSocket);
 			DatagramPacket serverReceivedPacket = client.receiveDatagram(client.clientSocket); //The server ACK packets are data packets, they just also function as an acknowledge
 			//This needs verbosity in some places, see the server/ transferhandler for how it's done
 
-			for (int blockNum = 1; serverReceivedPacket.getData().length > 512, blockNum++){
-				if (!client.validateDataPacket(serverReceivedPacket.getData(), 0) return; // breaks if it's not valid
+			for (int blockNum = 1; serverReceivedPacket.getData().length > 512; blockNum++){
+				if (!client.validateDataPacket(serverReceivedPacket.getData(), 0)) return; // breaks if it's not valid
 				client.writeFile(filename, serverReceivedPacket.getData());
 				DatagramPacket ackPacket = client.generateAckDatagram(serverReceivedPacket.getPort(), blockNum); //generate the ack and wait for more data
-				client.sendDatagram(ackPacket, clientSocket);
+				client.sendDatagram(ackPacket, client.clientSocket);
 			}
-			if (!client.validateDataPacket(serverReceivedPacket.getData(), 0) return; // breaks if it's not valid //do this one more time after the for loop for the packet that's < 512 bytes
+			if (!client.validateDataPacket(serverReceivedPacket.getData(), 0)) return; // breaks if it's not valid //do this one more time after the for loop for the packet that's < 512 bytes
 			client.writeFile(filename, serverReceivedPacket.getData());
 		} else { //write request
 			//The code here will basically be a direct copy of the code in TFTPTRansferHandler's read request, because it's doing the same thing
